6090df8d7a7c4cc9e2092764b455be5adcebf2a5
lukemusu16 <56021197+lukemusu16@users.noreply.github.com>
2023-01-16 12:13:28 +0100

    Fixed the removal of Greece

--- EmojieJunkie-Game/Assets/Scripts/EmojiKeyboardController.cs
+++ EmojieJunkie-Game/Assets/Scripts/EmojiKeyboardController.cs
@@ -2,7 +2,7 @@ using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class EmojiKeyboardController : MonoBehaviour
 {
@@ -10,12 +10,21 @@ public class EmojiKeyboardController : MonoBehaviour
     private int cols = 30;
     private int spriteWidth = 20;
     private int spriteHeight = 20;
+
+    private GameObject backBTN;
+
     public Texture2D img;
 
     List<GameObject> emojis = new List<GameObject>();
 
     public void Start()
     {
+        backBTN = GameObject.Find("BackspaceBTN");
+        backBTN.GetComponent<Button>().onClick.AddListener(() => {
+            RemoveEmoji();
+        });
+        backBTN.SetActive(false);
+
         GameObject emojiPrefab = Resources.Load<GameObject>("Emoji");
         //Texture2D sourceImage = GetComponent<SpriteRenderer>().sprite.texture;
         int width = img.width;
@@ -30,8 +39,34 @@ public class EmojiKeyboardController : MonoBehaviour
                 Sprite emoji = Sprite.Create(img, new Rect(xPos, yPos, spriteWidth, spriteHeight), new Vector2(10f, 10f));
                 GameObject newSprite = Instantiate(emojiPrefab, GameObject.Find("Content").transform);
                 newSprite.GetComponent<UnityEngine.UI.Image>().sprite = emoji;
-                emojis.Add(newSprite);
+
+                newSprite.GetComponent<Button>().onClick.AddListener(() => {
+                    if (emojis.Count < 5)
+                    {
+                        GameObject selectedEmoji = Instantiate(emojiPrefab, GameObject.Find("EmojiContainer").transform);
+                        selectedEmoji.GetComponent<UnityEngine.UI.Image>().sprite = emoji;
+                        emojis.Add(selectedEmoji);
+                    }
+                });  
             }
         }
+    }
+
+    private void Update() 
+    {
+        if (emojis.Count > 0)
+        {
+            backBTN.SetActive(true);
+        }
+        else
+        { 
+            backBTN.SetActive(false);
+        }
+    }
+
+    private void RemoveEmoji()
+    {
+        Destroy(emojis[emojis.Count - 1]);
+        emojis.RemoveAt(emojis.Count - 1);
     }
 }
