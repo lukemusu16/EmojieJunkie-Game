86fb55430cde0f50fbcbc08cbcf3fbc25ec7945e
lukemusu16 <56021197+lukemusu16@users.noreply.github.com>
2023-01-16 11:53:56 +0100

    Created Emoji Keyboard

--- EmojieJunkie-Game/Assets/Scripts/EmojiKeyboardController.cs
+++ EmojieJunkie-Game/Assets/Scripts/EmojiKeyboardController.cs
@@ -2,17 +2,21 @@ using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EmojiKeyboardController : MonoBehaviour
 {
-    public int rows = 24;
-    public int cols = 21;
-    public int spriteWidth = 33;
-    public int spriteHeight = 33;
+    private int rows = 30;
+    private int cols = 30;
+    private int spriteWidth = 20;
+    private int spriteHeight = 20;
     public Texture2D img;
+
+    List<GameObject> emojis = new List<GameObject>();
 
     public void Start()
     {
+        GameObject emojiPrefab = Resources.Load<GameObject>("Emoji");
         //Texture2D sourceImage = GetComponent<SpriteRenderer>().sprite.texture;
         int width = img.width;
         int height = img.height;
@@ -23,10 +27,10 @@ public class EmojiKeyboardController : MonoBehaviour
             {
                 int xPos = x * spriteWidth;
                 int yPos = y * spriteHeight;
-                Sprite emoji = Sprite.Create(img, new Rect(xPos, yPos, spriteWidth, spriteHeight), new Vector2(0.5f, 0.5f));
-                GameObject newSprite = new GameObject();
-                newSprite.AddComponent<SpriteRenderer>().sprite = emoji;
-                newSprite.transform.parent = GameObject.Find("EmojiKeyboard").transform.GetChild(0).GetChild(0).transform;
+                Sprite emoji = Sprite.Create(img, new Rect(xPos, yPos, spriteWidth, spriteHeight), new Vector2(10f, 10f));
+                GameObject newSprite = Instantiate(emojiPrefab, GameObject.Find("Content").transform);
+                newSprite.GetComponent<UnityEngine.UI.Image>().sprite = emoji;
+                emojis.Add(newSprite);
             }
         }
     }
